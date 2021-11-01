1fcaa758548a48c82850e164f1b362e51483c44c
Vimii <vimii8003@gmail.com>
2021-11-01 12:17:10 +0900

    start

---
+++ cmake-build-debug/CMakeFiles/Spectral_RayTracing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spectral_RayTracing.dir/main.cpp.obj"
+  "Spectral_RayTracing.exe"
+  "Spectral_RayTracing.exe.manifest"
+  "Spectral_RayTracing.pdb"
+  "libSpectral_RayTracing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spectral_RayTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
